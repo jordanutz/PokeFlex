e196bbf4dbe1af447c931a9b0643dfc237126b85
jordanutz <jordanutz@outlook.com>
2019-01-27 19:31:14 -0700

    Set up background object and use props to render different background depending on type of Pokemon

--- src/Components/Pokedex/Pokemon/Pokemon.js
+++ src/Components/Pokedex/Pokemon/Pokemon.js
@@ -5,8 +5,27 @@ const Pokemon = (props) => {
 
   console.log(props)
 
+  let background = {
+    "grass": "#41ED27",
+    "fire": "#ED4E27",
+    "water": "#1074F9",
+    "poison": "#A80BE8",
+    "bug": "#C6E80B",
+    "electric": "#FFF407",
+    "normal": "#FFEBCD",
+    "psychic": "#FF197B",
+    "ice": "#20AEF3",
+    "flying": "#FFEBCD",
+    "fairy": "#FF5EDB",
+    "ground": "#CD853F",
+    "rock": "#A0522D",
+    "ghost": "#A80BE8",
+    "dragon": "#4101FF",
+    "fighting": "#DEB887"
+  }
+
   return (
-    <div className="Pokemon">
+    <div className="Pokemon" style={{background: background[props.type]}}>
       <img src={props.sprite} />
     </div>
   )
